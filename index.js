a78261926a413467194d231f40273e41bfb0eae7
arjun <arjunmadikai996@gmail.com>
2025-03-12 15:29:11 +0400

    tournament id field added

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ document.getElementById("whatsappForm").addEventListener("submit", function (eve
     let teamName = document.getElementById("teamName").value;
     let leaderName = document.getElementById("leaderName").value;
     let leaderPhone = document.getElementById("leaderPhone").value;
+    let tournamentId = document.getElementById("tourNumber").value;
     
-    let message = `*RDM Gaming Hub - Team Registration*%0A----------------%0A*Team Name:* ${teamName}%0A*Leader Name:* ${leaderName}%0A*Phone:* ${leaderPhone}`;
+    let message = `*RDM Gaming Hub - Team Registration*%0A----------------%0A*Team Name:* ${teamName}%0A*Leader Name:* ${leaderName}%0A*Tournament ID:* ${tournamentId}%0A*Phone:* ${leaderPhone}`;
     
     let adminNumber = "919061089106"; // Replace with admin's WhatsApp number (without +)
     window.open(`https://wa.me/${adminNumber}?text=${message}`, "_blank");
